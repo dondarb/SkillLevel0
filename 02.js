cd9fc86bb0f10289659bac5db635401b4b014f49
dondarb <daalruba@gmail.com>
2024-01-14 20:02:43 -0500

    cambio de divisas clp a ars

--- 02.js
+++ 02.js
@@ -6,6 +6,18 @@
 * 		The system should ask the user if they want to perform another operation. If they choose to do so, it should restart the process; otherwise, the system should close.
 */
 
-let CLP, ARS, USD, EUR, TRY, GBP;
+let Cclp, ars, usd, eur, tri, gbp = 0;
 
-let menuConv = promt("Bievenido a su conversor de divisas\n" + "Que moneda inicial desea convertir\n"+"1. CLP Peso Chileno\n"+"2. ARS Peso Argentino"+"3. USD Dolares"+"4. EUR Euros"+"5. TRY Lira Turca"+"6. GBP Lira Esterlina");
+let menuConv = prompt("Bievenido a su conversor de divisas\n" + "Que moneda inicial desea convertir\n"+"1. CLP Peso Chileno\n"+"2. ARS Peso Argentino\n"+"3. USD Dolares\n"+"4. EUR Euros\n"+"5. TRY Lira Turca\n"+"6. GBP Lira Esterlina\n");
+
+if (menuConv == "1") {
+    let mon1 = prompt("Escoja divisa a convertir\n" + "1. ARS Peso Argentino\n"+"2. USD Dolares\n"+"3. EUR Euros\n"+"4. TRY Lira Turca\n"+"5. GBP Lira Esterlina\n");
+    if (mon1 == "1"){
+        clp = prompt("Valor a convertir");
+            let clp1 = parseInt(clp) * 0.89829;
+                alert(clp + " CLP" + " = " + clp1 + " ARS");
+                menuConv = prompt("Bievenido a su conversor de divisas\n" + "Que moneda inicial desea convertir\n"+"1. CLP Peso Chileno\n"+"2. ARS Peso Argentino\n"+"3. USD Dolares\n"+"4. EUR Euros\n"+"5. TRY Lira Turca\n"+"6. GBP Lira Esterlina\n");
+
+    }
+    
+}
